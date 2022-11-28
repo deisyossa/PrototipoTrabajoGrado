1c9b7d13effbf0ef9363cdf60050fce73e7809c7
Jeison Velásquez Sepúlveda <vsjeison@gmail.com>
2022-11-27 21:14:41 -0500

    Feature: Implementacion estable

--- src/main.py
+++ src/main.py
@@ -36,5 +36,44 @@ def show_municipios():
         municipios = explorer_service.generate_municipios()
         return render_template('municipios.html',municipios_image=municipios)
 
+@app.route('/subtemas/')
+def show_subtemas():
+        explorer_service = explorer(stopwords_es)
+        subtemas = explorer_service.generate_subtemas()
+        return render_template('subtemas.html',subtemas_image=subtemas)
+
+@app.route('/temas/')
+def show_temas():
+        explorer_service = explorer(stopwords_es)
+        temas = explorer_service.generate_temas()
+        return render_template('temas.html',temas_image=temas)
+
+@app.route('/servicios/')
+def show_servicios():
+        explorer_service = explorer(stopwords_es)
+        servicios = explorer_service.generate_servicios()
+        return render_template('servicios.html',servicios_image=servicios)
+
+@app.route('/usuarios/')
+def show_usuarios():
+        explorer_service = explorer(stopwords_es)
+        grafico, tabla,bar = explorer_service.generate_usuarios()
+        return render_template('usuarios.html',grafico_image=grafico,
+                tabla_image=tabla, bar_image=bar)
+
+@app.route('/eventos/')
+def show_eventos():
+        explorer_service = explorer(stopwords_es)
+        dia, sura, eps, savia = explorer_service.generate_eventos()
+        return render_template('eventos.html', dia_image=dia, 
+                sura_image=sura, eps_image=eps,
+                savia_image=savia)
+
+@app.route('/topicos/')
+def show_topicos():
+        explorer_service = explorer(stopwords_es)
+        topicos = explorer_service.generate_topicos()
+        return render_template('topicos.html',topicos_image=topicos)
+
 if __name__ == '__main__':
         app.run(debug=True)
--- src/utils/explorer.py
+++ src/utils/explorer.py
@@ -1,11 +1,9 @@
 """
 Modulo para carga de archivo CSV
 """
-#import datetime, spacy
 import re
 import os
-#import calendar
-#from time import time
+from time import time
 from nltk.corpus import stopwords
 from nltk.tokenize.toktok import ToktokTokenizer
 from nltk.tokenize import TweetTokenizer
@@ -16,10 +14,10 @@ import pandas as pd
 import numpy as np
 from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 from PIL import Image
-#from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer, TfidfVectorizer
-#from sklearn.decomposition import LatentDirichletAllocation
-#from sklearn.pipeline import Pipeline
-#from collections import Counter
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer, TfidfVectorizer
+from sklearn.decomposition import LatentDirichletAllocation
+from sklearn.pipeline import Pipeline
+from collections import Counter
 import seaborn as sns
 import matplotlib
 matplotlib.use('Agg')
@@ -177,7 +175,7 @@ class explorer():
         image_html = 'data:image/png;base64,{}'.format(comunas_b64_image)
         os.remove("temps/comunas.png")
         return image_html
-    
+
     def generate_municipios(self):
         """
         Genera una tabla con los municipios mas afectados
@@ -190,3 +188,272 @@ class explorer():
         image_html = 'data:image/png;base64,{}'.format(municipios_b64_image)
         os.remove("temps/municipios.png")
         return image_html
+
+    def generate_subtemas(self):
+        """
+        Genera una tabla con los subtemas tratados
+        """
+        df_sum_tema = self.df_atenciones.groupby('sub_tema').count().reset_index()
+        df_sum_tema = df_sum_tema.sort_values(by='sub_tema', ascending=True)[['sub_tema','solucion']].head(50)
+        dfi.export(df_sum_tema, 'temps/subtemas.png', dpi=200, max_cols=2)
+        with open("temps/subtemas.png", 'rb') as subtemas_image:
+            subtemas_b64_image = base64.b64encode(subtemas_image.read()).decode('utf-8')
+        image_html = 'data:image/png;base64,{}'.format(subtemas_b64_image)
+        os.remove("temps/subtemas.png")
+        return image_html
+
+    def generate_temas(self):
+        """
+        Genera una tabla con los temas principales tratados
+        """
+        df_sum_tema = self.df_atenciones.groupby('tema').count().reset_index()
+        df_sum_tema = df_sum_tema.sort_values(by='tema', ascending=True)[['tema','solucion']].head(50)
+        dfi.export(df_sum_tema, 'temps/temas.png', dpi=200, max_cols=2)
+        with open("temps/temas.png", 'rb') as temas_image:
+            temas_b64_image = base64.b64encode(temas_image.read()).decode('utf-8')
+        image_html = 'data:image/png;base64,{}'.format(temas_b64_image)
+        os.remove("temps/temas.png")
+        return image_html
+
+    def generate_servicios(self):
+        """
+        Genera una tabla con los servicios atendidos
+        """
+        df_servicios = self.df_atenciones.groupby('servicio').count().reset_index()
+        df_servicios = df_servicios.sort_values(by='servicio', ascending=True)[['servicio','solucion']].head(50)
+        dfi.export(df_servicios, 'temps/servicios.png', dpi=400, max_cols=2)
+        with open("temps/servicios.png", 'rb') as servicios_image:
+            servicios_b64_image = base64.b64encode(servicios_image.read()).decode('utf-8')
+        image_html = 'data:image/png;base64,{}'.format(servicios_b64_image)
+        os.remove("temps/servicios.png")
+        return image_html
+
+    def generate_usuarios(self):
+        """
+        Genera los graficos y tablas de los La cantidad de
+        usuarios únicos que publican realizan solicitudes
+        puede ayudar a identificar situaciones que provocaron
+        mayores picos de participación en el tiempo. 
+        """
+        tokenizer = TweetTokenizer()
+        ngram = 3
+        buffer = BytesIO()
+        df_atenciones_filtro = self.filter()
+        df_atenciones_filtro['tokenize'] = df_atenciones_filtro['hechos_filtrados'] \
+            .apply(tokenizer.tokenize)
+        df_atenciones_filtro['ngram'] = df_atenciones_filtro['tokenize'].apply(lambda x: list(ngrams(x, ngram)))
+        df_unique_users = df_atenciones_filtro.groupby(['dia_inicio'])['sub_tema'].nunique()
+        df_unique_users = df_unique_users.reset_index().sort_values('sub_tema', ascending=False).head(5)
+        dfi.export(df_unique_users, 'temps/tabla.png', dpi=200, max_cols=2)
+        with open("temps/tabla.png", 'rb') as servicios_image:
+            servicios_b64_image = base64.b64encode(servicios_image.read()).decode('utf-8')
+        tabla_html = 'data:image/png;base64,{}'.format(servicios_b64_image)
+        os.remove("temps/tabla.png")
+        plt.show()
+        fig = plt.figure(figsize=(15,5))
+        sns.lineplot(x='dia_inicio', y='sub_tema', data=df_unique_users.reset_index())
+        plt.title('Sub temas no repetidos por dia')
+        plt.ylabel('Sub Tema')
+        plt.xlabel('Fecha')
+        plt.savefig(buffer, format='png', facecolor=fig.get_facecolor())
+        buffer.seek(0)
+        base64_image = base64.b64encode(buffer.read()).decode('utf-8')
+        grafico_html = 'data:image/png;base64,{}'.format(base64_image)
+        df_atenciones_exploded = df_atenciones_filtro \
+            .explode('ngram')[['dia_inicio','ano_inicio','ngram']]
+        plt.close('all')
+        bar_html = self.generate_bar_usuario(df_atenciones_exploded)
+        return grafico_html, tabla_html, bar_html    
+
+    def generate_bar_usuario(self, df_atenciones_exploded):
+        """
+        Genera el grafico de barras
+        """
+        df_top_unique_days = df_atenciones_exploded[df_atenciones_exploded['dia_inicio'].isin(['2020-10-14','2020-10-15'])] \
+            .groupby('ngram') \
+            .count() \
+            .reset_index() \
+            .sort_values('dia_inicio', ascending=False) \
+            .head(10)
+        plt.show()
+        fig = plt.figure(figsize=(10,5))
+        sns.barplot(x='dia_inicio', y='ngram', data=df_top_unique_days, orient='h')
+        plt.title('Top Trigramas para 14 y 15/11/2020')
+        plt.xlabel('Menciones')
+        plt.ylabel('Trigrama')
+        buffer = BytesIO()
+        plt.savefig(buffer, format='png', facecolor=fig.get_facecolor())
+        buffer.seek(0)
+        base64_image = base64.b64encode(buffer.read()).decode('utf-8')
+        bar_html = 'data:image/png;base64,{}'.format(base64_image)
+        return bar_html
+
+    @staticmethod
+    def get_df_from_criteria(df, criteria):
+        df = df[df['hechos_filtrados'] \
+            .str \
+            .contains(criteria, flags=re.IGNORECASE, regex=True)] \
+            .groupby(['dia_inicio','ano_inicio','mes_inicio'], as_index=False) \
+            .agg(['count'])['fecha_inicio'].reset_index().rename(columns={'count':'hechos'})
+        return df.copy()
+
+    def generate_eventos(self):
+        df_atenciones_filtro = self.filter()
+
+        filtro_incapacidad = r'\bincapacidad|\blicencia|\binasistencia'
+        incapacidad_x_dia_df = self.get_df_from_criteria(df_atenciones_filtro, filtro_incapacidad)
+
+        filtro_eps = r'\beps|\bentidad|\bprestadora|\bsalud'
+        eps_x_dia_df = self.get_df_from_criteria(df_atenciones_filtro, filtro_eps)
+
+        filtro_savia = r'\bsavia'
+        savia_x_dia_df = self.get_df_from_criteria(df_atenciones_filtro, filtro_savia)
+
+        filtro_sura = r'\bsura'
+        sura_x_dia_df = self.get_df_from_criteria(df_atenciones_filtro, filtro_sura)   
+
+        dia_html = self.get_grafico_dia(incapacidad_x_dia_df,
+            eps_x_dia_df,savia_x_dia_df,sura_x_dia_df)
+        sura_html = self.get_grafico_sura(sura_x_dia_df)
+        eps_html = self.get_grafico_eps(eps_x_dia_df)
+        savia_html = self.get_grafico_savia(savia_x_dia_df)
+        return dia_html, sura_html, eps_html, savia_html
+        
+    @staticmethod
+    def get_grafico_dia(incapacidad_x_dia_df,eps_x_dia_df,
+            savia_x_dia_df,sura_x_dia_df):
+        plt.show()
+        fig = plt.figure(figsize=(15,6))
+        ax = fig.add_subplot(111)
+        sns.lineplot(x='dia_inicio', y='hechos', data=incapacidad_x_dia_df[incapacidad_x_dia_df['mes_inicio'].isin([9,10,11])], ax=ax)
+        sns.lineplot(x='dia_inicio', y='hechos', data=eps_x_dia_df[eps_x_dia_df['mes_inicio'].isin([9,10,11])], ax=ax)
+        sns.lineplot(x='dia_inicio', y='hechos', data=sura_x_dia_df[sura_x_dia_df['mes_inicio'].isin([9,10,11])], ax=ax)
+        sns.lineplot(x='dia_inicio', y='hechos', data=savia_x_dia_df[savia_x_dia_df['mes_inicio'].isin([9,10,11])], ax=ax)
+        plt.title('Cantidad de Menciones por Dia')
+        plt.ylabel('Menciones')
+        plt.xlabel('Fecha')
+        ax.legend(['incapacidad', 'eps', 'savia', 'sura'], loc='upper left', prop={'size': 12})
+        buffer = BytesIO()
+        plt.savefig(buffer, format='png', facecolor=fig.get_facecolor())
+        buffer.seek(0)
+        base64_image = base64.b64encode(buffer.read()).decode('utf-8')
+        dia_html = 'data:image/png;base64,{}'.format(base64_image)
+        return dia_html
+
+    @staticmethod
+    def get_grafico_sura(sura_x_dia_df):
+        plt.show()
+        fig = plt.figure(figsize=(15,6))
+        sura_x_mes_df = sura_x_dia_df.groupby(['ano_inicio','mes_inicio']) \
+                    .sum().reset_index()[['mes_inicio','ano_inicio','hechos']]
+        sura_x_mes_pivot = sura_x_mes_df \
+                            .pivot(index='mes_inicio',columns='ano_inicio', values='hechos')
+        sura_x_mes_pivot.plot(kind='bar', figsize=(15, 6), color=['lightgray', 'gray', 'black'], rot=0)                                       
+        plt.title("Comparaciones intermensuales de menciones de sura")
+        plt.xlabel("Mes", labelpad=16)
+        plt.ylabel("Menciones (sura)", labelpad=16)
+        buffer = BytesIO()
+        plt.savefig(buffer, format='png', facecolor=fig.get_facecolor())
+        buffer.seek(0)
+        base64_image = base64.b64encode(buffer.read()).decode('utf-8')
+        sura_html = 'data:image/png;base64,{}'.format(base64_image)
+        return sura_html
+
+    @staticmethod
+    def get_grafico_eps(eps_x_dia_df):
+        plt.show()
+        fig = plt.figure(figsize=(15,6))
+        eps_x_mes_df = eps_x_dia_df.groupby(['ano_inicio','mes_inicio']) \
+                            .sum().reset_index()[['mes_inicio','ano_inicio','hechos']]
+        eps_x_mes_pivot = eps_x_mes_df \
+                            .pivot(index='mes_inicio',columns='ano_inicio', values='hechos')
+        eps_x_mes_pivot.plot(kind='bar', figsize=(15, 6), color=['lightgray', 'gray', 'black'], rot=0)                                       
+        plt.title("Comparaciones intermensuales de menciones de eps")
+        plt.xlabel("Mes", labelpad=16)
+        plt.ylabel("Menciones (eps)", labelpad=16)
+        buffer = BytesIO()
+        plt.savefig(buffer, format='png', facecolor=fig.get_facecolor())
+        buffer.seek(0)
+        base64_image = base64.b64encode(buffer.read()).decode('utf-8')
+        eps_html = 'data:image/png;base64,{}'.format(base64_image)
+        return eps_html
+
+    @staticmethod
+    def get_grafico_savia(savia_x_dia_df):
+        plt.show()
+        fig = plt.figure(figsize=(15,6))
+        savia_x_mes_df = savia_x_dia_df.groupby(['ano_inicio','mes_inicio']) \
+                    .sum().reset_index()[['mes_inicio','ano_inicio','hechos']]
+        savia_x_mes_pivot = savia_x_mes_df \
+                            .pivot(index='mes_inicio',columns='ano_inicio', values='hechos')
+        savia_x_mes_pivot.plot(kind='bar', figsize=(15, 6), color=['lightgray', 'gray', 'black'], rot=0)                                       
+        plt.title("Comparaciones intermensuales de menciones de Savia EPS")
+        plt.xlabel("Mes", labelpad=16)
+        plt.ylabel("Menciones (Savia)", labelpad=16)
+        buffer = BytesIO()
+        plt.savefig(buffer, format='png', facecolor=fig.get_facecolor())
+        buffer.seek(0)
+        base64_image = base64.b64encode(buffer.read()).decode('utf-8')
+        savia_html = 'data:image/png;base64,{}'.format(base64_image)
+        return savia_html
+
+    def generate_topicos(self):
+        """
+        Existen diferentes técnicas de identificación de temas o tópicos pero una de las más utilizadas es
+        Latent Dirichlet Allocation o LDA. Se trata de una técnica que genera un modelo probabilístico que
+        asume que cada tema es una combinación de palabras y que cada documento (o caso en este escenario) es
+        una combinación de temas con diferentes probabilidades.
+
+        En las celdas de abajo, se crea un Pipeline que primero aplica una técnica conocida como TF-IDF que 
+        calcula la frecuencia de palabras en los casos, y calcula un score para cada palabra dando menos 
+        importancia a aquellas que aparecen con demasiada frecuencia y son poco relevantes. En el siguiente 
+        paso del pipeline se entrena el modelo y transforma el dataframe original. En este caso, else elije
+        clasificar los temas en 5 diferentes categorias pero el número puede variar de acuerdo al caso. 
+        Existen formas de medir la calidad del modelo, calculando lo que se denomina perplexity pero no se va
+        a utilizar en este caso.
+        """
+        n_topics = 5
+        df_atenciones_filtro = self.filter()
+        text_pipeline = Pipeline([
+            ('tfidf', TfidfVectorizer(ngram_range=(2,3), min_df=100, max_df=0.85)),
+            ('lda', LatentDirichletAllocation(n_components=n_topics, max_iter=5, learning_method='online'))
+        ])
+        t0 = time()
+        lda_model = text_pipeline.fit_transform(df_atenciones_filtro['hechos_filtrados'])
+        print(time() - t0)
+        tfidf = text_pipeline.steps[0][1]
+        lda = text_pipeline.steps[1][1]
+        vocabulario = tfidf.get_feature_names()
+        top_topics = 5
+        topic_dict = {}
+        topic_scores = []
+        for topic_idx, topic in enumerate(lda.components_):
+            topic_dict[str(topic_idx)] = ",".join([vocabulario[i] for i in topic.argsort()[:-top_topics - 1:-1]])
+            topic_scores.append([topic[i] for i in topic.argsort()[:-top_topics - 1:-1]])
+        df_topics_lda = pd.DataFrame(topic_dict, index=['bigrams'])
+        df_topics_lda = df_topics_lda.T.reset_index()
+        df_topics_names = pd.DataFrame(df_topics_lda.bigrams.str.split(',').tolist(), index=df_topics_lda.index) \
+                    .stack() \
+                    .reset_index() \
+                    .drop(['level_1'], axis=1) \
+                    .rename(columns={0:'bigrams', 'level_0':'topic'})
+        df_topics_scores = pd.DataFrame(topic_scores) \
+                .stack() \
+                .reset_index(drop=True)
+        df_topics = pd.concat([df_topics_names, df_topics_scores], axis=1) \
+                .rename(columns={0:'score'})
+        plt.show()
+        fig = plt.figure(figsize=(12,10))
+        for i in range(1,n_topics+1):    
+            plt.subplot(3,2,i,frameon=True)
+            sns.barplot(x='score', y='bigrams', data=df_topics[df_topics['topic']==i-1], orient='h')
+            plt.title("Topico {}".format(i))
+            plt.xlabel('')
+            plt.ylabel('')
+            plt.tight_layout()
+        buffer = BytesIO()
+        plt.savefig(buffer, format='png', facecolor=fig.get_facecolor())
+        buffer.seek(0)
+        base64_image = base64.b64encode(buffer.read()).decode('utf-8')
+        topicos_html = 'data:image/png;base64,{}'.format(base64_image)
+        return topicos_html
